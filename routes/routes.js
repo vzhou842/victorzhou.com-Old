a60c85d9646654cddac60106b45a0c755031d3f8
Victor Zhou <vzhou842@gmail.com>
2015-12-25 00:49:06 -0600

    added /iOS, /Android, /Web, /Research

--- routes/routes.js
+++ routes/routes.js
@@ -16,5 +16,17 @@ router.get('/cloak', function(req, res, next) {
 router.get('/privacy/encircle2', function(req, res, next) {
   res.sendFile(global.appRoot + '/public/privacy-encircle2.html');
 });
+router.get('/iOS', function(req, res, next) {
+	res.redirect(req.protocol + '://' + req.get('host') + '/projects#iOS');
+});
+router.get('/Android', function(req, res, next) {
+	res.redirect(req.protocol + '://' + req.get('host') + '/projects#Android');
+});
+router.get('/Web', function(req, res, next) {
+	res.redirect(req.protocol + '://' + req.get('host') + '/projects#Web');
+});
+router.get('/Research', function(req, res, next) {
+	res.redirect(req.protocol + '://' + req.get('host') + '/projects#Research');
+})
 
 module.exports = router;
