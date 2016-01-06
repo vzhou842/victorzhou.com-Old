5510e7e8e4e70d54cea9b4d191fb35dff547415c
Victor Zhou <vzhou842@gmail.com>
2016-01-06 14:45:08 -0500

    Encircle2: added POST /createMap

--- encircle2/routes.js
+++ encircle2/routes.js
@@ -46,4 +46,46 @@ router.get('/newMaps', function(req, res, next) {
 	});
 });
 
+/** POST /createMap
+ * Creates a custom map.
+ * @param map The map string.
+ * @param title The title of the map.
+ * @param description The description of the map.
+ * @param creator OPTIONAL. The creator of the map.
+ */
+ router.post('/createMap', function(req, res, next) {
+ 	var map = req.body.map;
+ 	var title = req.body.title;
+ 	var description = req.body.description;
+ 	var creator = req.body.creator;
+ 	if (!creator) creator = 'Unknown';
+
+ 	if (!map) {
+ 		res.status(400).json({'error_message' : "'map' is a required param."});
+ 		return;
+ 	} else if (!title) {
+ 		res.status(400).json({'error_message' : "'title' is a required param."});
+ 		return;
+ 	} else if (!description) {
+ 		res.status(400).json({'error_message' : "'description' is a required param."});
+ 		return;
+ 	}
+
+ 	var newMap = new EncircleMap({
+ 		map : map,
+ 		title : title,
+ 		description : description,
+ 		creator : creator,
+ 		createdAt : Date.now(),
+ 	});
+
+ 	newMap.save(function(err, savedMap) {
+ 		if (!err) {
+ 			res.status(200).json({'message' : 'Map successfully saved.'});
+ 		} else {
+ 			res.status(500).json({'error_message' : err.message});
+ 		}
+ 	});
+ });
+
 module.exports = router;
