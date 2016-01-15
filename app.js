d4a29da80de8d26788c68542a8f66ebb51af1a4e
Victor Zhou <vzhou842@gmail.com>
2016-01-14 21:27:58 -0500

    added express maxage caching

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxage: '4h' }));
 
 app.use('/', routes);
 app.use('/sendemail', sendemail);
