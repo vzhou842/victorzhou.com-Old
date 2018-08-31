98afdcbd1daaf4a2165d3be708cc7740dd3fd21b
Victor Zhou <vzhou842@gmail.com>
2018-08-31 13:41:57 -0500

    Updated to Nodemailer 4

--- routes/sendemail.js
+++ routes/sendemail.js
@@ -2,40 +2,43 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 
+if (!process.env.VZ_EMAIL_PASS) {
+  console.error('VZ_EMAIL_PASS env var not supplied!');
+}
+
 // create reusable transporter object using SMTP transport
 var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'vzhou842@gmail.com',
-        pass: process.env.VZ_EMAIL_PASS,
-    }
+  service: 'Gmail',
+  auth: {
+    user: 'omgvictory@gmail.com',
+    pass: process.env.VZ_EMAIL_PASS,
+  }
 });
 
 /* POST sendemail */
 router.post('/', function(req, res, next) {
-	console.log('query: ' + JSON.stringify(req.query) + ' body: ' + JSON.stringify(req.body));
-	var name = req.body.name;
-	var email = req.body.email;
-	var message = req.body.message;
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
 
-	// setup e-mail data with unicode symbols
-	var mailOptions = {
-	    from: name, // sender address
-	    to: 'vzhou842@gmail.com', // list of receivers
-	    subject: 'Victorzhou.com Contact Submission', // Subject line
-	    text: 'name: ' + name + '\nemail: ' + email + '\nmessage: ' + message // plaintext body
-	};
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+    from: name, // sender address
+    to: 'vzhou842@gmail.com', // list of receivers
+    subject: 'Victorzhou.com Contact Submission', // Subject line
+    text: 'name: ' + name + '\nemail: ' + email + '\nmessage: ' + message // plaintext body
+  };
 
-	// send mail with defined transport object
-	transporter.sendMail(mailOptions, function(error, info){
-	    if (error){
-	        console.log(error);
-	        res.status(500).end();
-	        return;
-	    }
-	    console.log('Message sent: ' + info.response);
-	    res.status(200).end();
-	});
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error){
+      console.error(error);
+      res.status(500).end();
+      return;
+    }
+    console.log('Email sent: ' + info.response);
+    res.status(200).end();
+  });
 });
 
 module.exports = router;
